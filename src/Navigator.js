0e68b0eb59dae497ad2fb5fffd5708841be115fb
Félix Vicente <felixvicent1306@gmail.com>
2020-07-27 14:13:11 -0300

    Add screen Login

--- src/Navigator.js
+++ src/Navigator.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 import Feed from './screens/Feed';
 import AddPhoto from './screens/AddPhoto';
 import Profile from './screens/Profile';
+import Login from './screens/Login';
 
 const Tab = createBottomTabNavigator();
+const LoginOrProfile = createStackNavigator();
+
+function LoginOrProfileNavigator(){
+  return (
+    <LoginOrProfile.Navigator
+      initialRouteName='Profile'
+      headerMode='none'
+    >
+      <LoginOrProfile.Screen name="Profile" component={ Profile } />
+      <LoginOrProfile.Screen name="Auth" component={ Login } />
+    </LoginOrProfile.Navigator>
+  )
+}
 
 export default function Navigator(){
   return(
@@ -30,7 +45,7 @@ export default function Navigator(){
         />
         <Tab.Screen
           name='Profile'
-          component={ Profile }
+          component={ LoginOrProfileNavigator }
           options={{tabBarIcon: ({ color }) => <Icon name="user" size={ 30 } color={ color } />}}
         />
       </Tab.Navigator>
--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -4,7 +4,7 @@ import { Gravatar } from 'react-native-gravatar';
 
 class Profile extends Component {
   logout = () => {
-
+    this.props.navigation.replace('Auth')
   }
 
   render(){
