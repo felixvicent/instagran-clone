5477df011441adc8f5e4823b67686b23a4d8dd57
Félix Vicente <felixvicent1306@gmail.com>
2020-07-28 12:55:29 -0300

    Add user info to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Platform, Image } from 'react-native';
+import { connect } from 'react-redux';
+import { Gravatar } from 'react-native-gravatar';
 
 import icon from '../../assets/imgs/icon.png';
 
 class Header extends Component {
   render(){
+    const name = this.props.name || 'Anonymous';
+    const gravatar = this.props.email ? 
+      <Gravatar
+        options={{
+          email: this.props.email,
+          secure: true,
+        }}
+        style={ styles.avatar }
+      /> : null;
     return(
       <View style={ styles.container }>
         <View style={ styles.rowContainer } >
           <Image source={ icon } style={ styles.image } />
           <Text style={ styles.title } >Lambe Lambe</Text>
+        </View>
+        <View style={ styles.userContainer } >
+          <Text style={ styles.user } >{ name }</Text>
+          { gravatar }
         </View>
       </View>
     );
@@ -23,6 +38,8 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderColor: '#ddd',
     width: '100%',
+    flexDirection :'row',
+    justifyContent: 'space-between'
   },
   rowContainer: {
     flexDirection: 'row',
@@ -38,7 +55,27 @@ const styles = StyleSheet.create({
     fontFamily: 'shelter',
     height: 30,
     fontSize: 28,
+  },
+  userContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  user: {
+    fontSize: 10,
+    color: '#888',
+  },
+  avatar: {
+    width: 30,
+    height: 30,
+    marginLeft: 10,
   }
-})
+});
 
-export default Header;
+const mapStateToProps = ({ user }) => {
+  return {
+    email: user.email,
+    name: user.name,
+  }
+}
+
+export default connect(mapStateToProps)(Header);
